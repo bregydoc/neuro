368d9d0384bac88b68db90184136c468bfb7e7a6
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-05-12 23:41:22 -0500

    added state of the art

--- presentation/index.js
+++ presentation/index.js
@@ -3,70 +3,86 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
+	BlockQuote,
+	Cite,
+	Deck,
+	Heading,
+	ListItem,
+	List,
+	Quote,
+	Slide,
+	Text
 } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
-// Require CSS
-require("normalize.css");
+import "normalize.css";
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+	{
+		primary: "white",
+		secondary: "#FF5368",
+		tertiary: "#00E0C4",
+		quarternary: "#FFD981"
+	},
+	{
+		primary: "Montserrat",
+		secondary: "Helvetica"
+	}
+);
 
 export default class Presentation extends React.Component {
-  render() {
-    return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
-      </Deck>
-    );
-  }
+	render() {
+		return (
+			<Deck
+				transition={["zoom", "slide"]}
+				transitionDuration={500}
+				theme={theme}
+				progress="bar"
+			>
+				<Slide transition={["zoom"]} bgColor="primary">
+					<Heading
+						size={1}
+						fit
+						caps
+						lineHeight={1}
+						textColor="secondary"
+					>
+						Neuro Research
+					</Heading>
+					<Text textColor="tertiary" size={2} fit>
+						Research (or not) about EEG and classification
+						algorithms
+					</Text>
+				</Slide>
+				<Slide transition={["slide"]} bgColor="tertiary">
+					<Heading
+						size={1}
+						fit
+						caps
+						lineHeight={1}
+						textColor="primary"
+					>
+						State of the art
+					</Heading>
+				</Slide>
+				<Slide>
+					<Heading size={3} caps textColor="secondary">
+						Classification of EEG with Recurrent Neural Networks
+					</Heading>
+				</Slide>
+				<Slide>
+					<Heading fit caps bold textColor="tertiary">
+						What they did?
+					</Heading>
+					<Text margin="40px 0 0 0" lineHeight={1}>
+						They tried to classify whether someone is viewing a 2D
+						or 3D image (DP3). With emphasis on Neural Networks
+						Classificators, particulary Recurrent Neural Networks.
+					</Text>
+				</Slide>
+			</Deck>
+		);
+	}
 }
